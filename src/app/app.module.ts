8d249d61deeb25f0b4d02474d2c84e952b538168
BuntyBru <buntybru@gmail.com>
2018-09-15 10:06:45 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
+import { HeaderComponent } from './header/header.component';
+import { InterestsComponent } from './interests/interests.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkComponent } from './work/work.component';
+import { BtechComponent } from './education/btech/btech.component';
+import { TenComponent } from './education/ten/ten.component';
+import { TwelveComponent } from './education/twelve/twelve.component';
+import { EmailComponent } from './header/email/email.component';
+import { NameComponent } from './header/name/name.component';
+import { NetworkComponent } from './work/network/network.component';
+import { SdeComponent } from './work/sde/sde.component';
+import { TechComponent } from './work/tech/tech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    HeaderComponent,
+    InterestsComponent,
+    LanguagesComponent,
+    ProjectComponent,
+    SkillsComponent,
+    WorkComponent,
+    BtechComponent,
+    TenComponent,
+    TwelveComponent,
+    EmailComponent,
+    NameComponent,
+    NetworkComponent,
+    SdeComponent,
+    TechComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/work/sde/sde.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sde',
+  templateUrl: './sde.component.html',
+  styleUrls: ['./sde.component.css']
+})
+export class SdeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
